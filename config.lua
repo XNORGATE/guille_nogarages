16cdf3d43bff3648cbac96f931d0d2a1d33e05e5
guillerp <72713057+guillerp8@users.noreply.github.com>
2021-02-13 20:06:12 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,5 +2,5 @@ Config = {}
 
 Config.savetime = 5 -- In seconds (IMPORTANT)
 Config.moneytoretrieve = 100 -- Money to get back the vehicle.
-Config.timetospawn = 20 -- In seconds (IMPORTANT) (I RECOMMEND NOT TOUCHING THIS VALUE) When the first player is loaded the vehicles will load. It requieres a time to 20 seconds to load correctly the player and start the spawn.
+Config.timetospawn = 40 -- In seconds (IMPORTANT) (I RECOMMEND NOT TOUCHING THIS VALUE) When the first player is loaded the vehicles will load. It requieres a time to 20 seconds to load correctly the player and start the spawn.
 
