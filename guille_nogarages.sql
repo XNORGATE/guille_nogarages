f64be2b34899c9d4bfd699ad961132cabdb838dc
guillerp <72713057+guillerp8@users.noreply.github.com>
2021-02-13 19:34:43 +0100

    Update guille_nogarages.sql

--- guille_nogarages.sql
+++ guille_nogarages.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `owned_vehicles` ADD `position` LONGTEXT NOT NULL DEFAULT '{"z":-3.88,"heading":95,"x":-3045,"y":7807}';
+ALTER TABLE `owned_vehicles` ADD `position` LONGTEXT NOT NULL DEFAULT '{"z":-3.88,"heading":95.00,"x":-3045.00,"y":7807.00}';
